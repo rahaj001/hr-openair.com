761660de46da5bc16c01c7ad088619daead9d942
Rabii <rabii@hotmail.de>
2025-07-02 20:13:09 +0200

    Fix routing base for Vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: '/', // wichtig für richtiges Routing
   build: {
     outDir: 'dist',
   },
