f7881bc953eef79a490aef139c3cbeb4fafadf19
Rabii <rabii@hotmail.de>
2025-07-17 20:08:11 +0200

    neu server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
-//import dotenv from "dotenv";
-require("dotenv").config();
+import dotenv from "dotenv";
+
 import contactRoute from "./routes/contact.js";
 
 dotenv.config();
