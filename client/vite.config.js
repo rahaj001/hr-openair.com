0a906579a69c4828e572dc54bbb71a90b575bbaf
rahaj001 <130575246+rahaj001@users.noreply.github.com>
2025-08-14 21:28:58 +0200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/apps/', // wichtig für richtiges Routing
+   base: '/', // wichtig für richtiges Routing
   build: {
     outDir: 'dist',
   },
