c4c7dc8fca1532994e9c334a0f886406891b72f7
rahaj001 <130575246+rahaj001@users.noreply.github.com>
2025-08-14 20:59:55 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,16 @@
-import { useContext } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { LanguageContext } from "./components/language/LanguageContext";
-import Home from './pages/Home';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ScrollToTop from './components/ScrollToTop';
-import Kontakt from './pages/Kontakt';
-import OpenairFocus from './pages/OpenairFocus';
-import CypherboxInfos from './pages/CypherboxInfos';
-import Impressum from "./pages/Impressum";
-import Datenschutz from "./pages/Datenschutz";
-import CookiesRechlinie from './pages/CoockiesRechlinie';
-import { ScrollProvider } from "./utils/ScrollContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { LanguageProvider } from "./components/language/LanguageContext";
+//import App from './App';
+//import './App.css';
 
-
-function App() {
-  const { lang } = useContext(LanguageContext);
-  
-  const onCtaClick = () => {
-    alert("Test Nachricht ......!");
-  };
-
-  return (
-      <ScrollProvider>
-    <div className="app-container">
-      <Header />
-      
-      <Routes>
-        <Route path="/" element={<Home />} />                                 
-        <Route path="/apps" element={<CypherboxInfos lang={lang} />} />                      
-        <Route path="/focus" element={<OpenairFocus  lang={lang} />} />                   
-        <Route path="/kontakt" element={<Kontakt />} />                              
-        <Route path="/featuresSection" element={<CypherboxInfos />} />
-        <Route path="/impressum" element={<Impressum lang={lang} />} />
-        <Route path="/datenschutz" element={<Datenschutz lang={lang} />} />
-        <Route path="/cookiesRechlinie" element={<CookiesRechlinie lang={lang} />} />
-      </Routes>
-      <ScrollToTop />
-      <Footer />
-      
-    </div>
-     </ScrollProvider>
-   
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <LanguageProvider>
+      <h1>Hallo Welt ..</h1>
+      </LanguageProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
