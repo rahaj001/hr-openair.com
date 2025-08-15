6b870d15403246b6d4538545fb5caf4c402350d0
rahaj001 <130575246+rahaj001@users.noreply.github.com>
2025-08-15 17:27:43 +0200

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { LanguageProvider } from "./components/language/LanguageContext";
-//import App from './App';
-//import './App.css';
+import App from './App';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <LanguageProvider>
-      <h1>Hallo Welt ..</h1>
+        <App />
       </LanguageProvider>
     </BrowserRouter>
   </React.StrictMode>
